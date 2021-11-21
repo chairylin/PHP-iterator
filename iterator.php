dfcd0d7588fc22067761f41f5e2a89509fc29429
chairylin <chairylin@mail.ru>
2021-11-21 17:11:10 +0300

    Corrected indents

--- iterator.php
+++ iterator.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SERVER['DOCUMENT_ROOT'] == '') {
-	$_SERVER['DOCUMENT_ROOT'] = __DIR__;
+    $_SERVER['DOCUMENT_ROOT'] = __DIR__;
 }
 
 $path = $_SERVER['DOCUMENT_ROOT'].'/docs/';
@@ -11,21 +11,21 @@ $res = [];
 
 function iterator($it, &$res)
 {
-	for ($it->rewind(); $it->valid(); $it->next()) {
-		if ($it->isDot()) {
-			continue;
-		}
-		if ($it->isDir()) {
-			iterator(new DirectoryIterator($it->current()->getPathName()), $res);
-		} else {
-			$re = '/^[0-9A-Za-z]{1,}\.ixt$/';
-			//$re = '/^[-_0-9A-Za-z]{1,}\.png$/';
-			$file_name = $it->current()->getFilename();
-			if ( preg_match($re, $file_name) ) {
-				$res[] = $file_name;
-			}
-		}
-	}
+    for ($it->rewind(); $it->valid(); $it->next()) {
+        if ($it->isDot()) {
+            continue;
+        }
+        if ($it->isDir()) {
+            iterator(new DirectoryIterator($it->current()->getPathName()), $res);
+        } else {
+            $re = '/^[0-9A-Za-z]{1,}\.ixt$/';
+            //$re = '/^[-_0-9A-Za-z]{1,}\.png$/';
+            $file_name = $it->current()->getFilename();
+            if ( preg_match($re, $file_name) ) {
+                $res[] = $file_name;
+            }
+        }
+    }
 }
 
 iterator($it, $res);
